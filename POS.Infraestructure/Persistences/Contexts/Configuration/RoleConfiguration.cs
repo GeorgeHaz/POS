642f9054b61674119cb033119d457d0429d55e0b
George Haz <george.haz@yahoo.com>
2025-03-19 15:17:31 -0500

    Agregar archivos de proyecto.

---
+++ POS.Infraestructure/Persistences/Contexts/Configuration/RoleConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using POS.Domain.Entities;
+
+namespace POS.Infraestructure.Persistences.Contexts.Configuration
+{
+    internal class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.HasKey(e => e.RoleId).HasName("PK__Roles__8AFACE1A2F7589D4");
+
+            builder.Property(e => e.Description)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        }
+    }
+}
---
+++ POS.Infraestructure/Persistences/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace POS.Infraestructure.Persistences.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //Decalracion o matricula de nuestras interfaces a nivel de repositorio
+        ICategoryRepository Category { get; }
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
